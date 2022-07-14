b8a054d725d530487d3d342f37806aaf9af4e82d
ameliaannisa <109267089+ameliaannisa@users.noreply.github.com>
2022-07-14 13:32:44 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,235 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <!-- Basic -->
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <!-- Mobile Metas -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <!-- Site Metas -->
+  <meta name="keywords" content="" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
+
+  <title>SiKolog</title>
+
+  <!-- slider stylesheet -->
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.1.3/assets/owl.carousel.min.css" />
+
+  <!-- bootstrap core css -->
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+
+  <!-- fonts style -->
+  <link href="https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap" rel="stylesheet" />
+  <!-- Custom styles for this template -->
+  <link href="css/style.css" rel="stylesheet" />
+  <link href="css/styler.css" rel="stylesheet" />
+  <!-- responsive style -->
+  <link href="css/responsive.css" rel="stylesheet" />
+</head>
+
+<body>
+  <div class="hero_area">
+    <!-- header section strats -->
+    <header class="header_section">
+      <div class="container">
+        <nav class="navbar navbar-expand-lg custom_nav-container pt-3">
+          <a class="navbar-brand" href="index.php">
+            <img src="images/lampuuu.png" alt="" /><span>
+              SiKolog
+            </span>
+          </a>
+        </nav>
+      </div>
+    </header>
+    <!-- end header section -->
+    <!-- slider section -->
+    <section class=" slider_section position-relative">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="slider_item-detail">
+              <div>
+                <h1>
+                  Welcome to SiKolog
+                </h1>
+                <p>
+                  di sini kamu bisa melakukan deteksi kesehatan mental kamu loh
+                  tapi sebelum itu baca langkah-langkah nya dulu ya!
+                </p>
+                <div class="d-flex">
+                  <a href="#langkah" class="text-uppercase custom_yellow-btn mr-3">
+                    Baca
+                  </a>
+                  <a href="home.php" class="text-uppercase custom_pink-btn">
+                    Langsung Mulai
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="slider_img-box">
+              <div>
+                <img src="images/ssampul.png" alt="" class="" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- end slider section -->
+  </div>
+
+  <!-- service section -->
+  <section class="service_section layout_padding ">
+    <div id="langkah">
+      <div class="container">
+        <h2 class="custom_heading">Langkah - Langkah</h2>
+        <p class="custom_heading-text">
+          Tahapan yang harus kamu pahami untuk melakukan tes kesehatan mental
+        </p>
+        <div class=" layout_padding2">
+          <div class="card-deck">
+            <div class="card">
+              <img class="card-img-top" src="images/a.png" alt="Card image cap" />
+              <div class="card-body">
+                <h5 class="card-title">Main Game</h5>
+                <p class="card-text">
+                  Pertama, kamu harus bermain games yang ada di aplikasi Funlearning dulu ya!
+                </p>
+              </div>
+            </div>
+            <div class="card">
+              <img class="card-img-top" src="images/b.png" alt="Card image cap" />
+              <div class="card-body">
+                <h5 class="card-title">Mulai Tes</h5>
+                <p class="card-text">
+                  Setelah itu kamu bisa melakukan sesi tes, dan jawab pertanyaannya sesuai dengan yang kamu rasakan
+                </p>
+              </div>
+            </div>
+            <div class="card">
+              <img class="card-img-top" src="images/c.png" alt="Card image cap" />
+              <div class="card-body">
+                <h5 class="card-title">Hasil Tes</h5>
+                <p class="card-text">
+                  Dan setelah kamu selesai melukan tes, hasil tes kamu akan ditampilkan
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="d-flex justify-content-center">
+          <a href="home.php" class="custom_yellow-btn">
+            Sudah Mengerti
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- end service section -->
+
+  <!-- client section -->
+
+  <section class="client_section layout_padding">
+    <div class="container">
+      <h2 class="custom_heading">- INFO -</h2>
+      <div>
+        <div id="carouselExampleControls-2" class="carousel slide" data-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <div class="client_container layout_padding2">
+                <div class="client_img-box">
+                  <img src="images/sikologg.png" alt="" />
+                </div>
+                <div class="client_detail">
+                  <h3>
+                    SiKolog
+                  </h3>
+                  <p class="custom_heading-text">
+                    SiKolog merupakan platform yang dibuat khusus untuk pengguna Funlearnig Digital agar depat melakukan diagnosa kesehatan mental yang sedang dialami saat bermain game dengan mandiri
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <div class="client_container layout_padding2">
+                <div class="client_img-box">
+                  <img src="images/fllogo.png" alt="" />
+                </div>
+                <div class="client_detail">
+                  <h3>
+                    Funlearning Digital
+                  </h3>
+                  <p class="custom_heading-text">
+                    Funlearning Digital merupakan hasil dari kolaborasi antara antara Disruptive Learning Innovation dan Funlearning id yang berupa sebuah produk edukasi digital yang berokus pada pengaplikasian sebuah games edukatif 2D, AR, dan VR, berbasis android untuk anak-anak
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="custom_carousel-control">
+            <a class="carousel-control-prev" href="#carouselExampleControls-2" role="button" data-slide="prev">
+              <span class="" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleControls-2" role="button" data-slide="next">
+              <span class="" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- end client section -->
+
+
+  <!-- footer section -->
+  <section class="container-fluid footer_section">
+    <p>
+      Copyright &copy; 2019 All Rights Reserved By
+      <a href="https://html.design/">Free Html Templates</a>
+    </p>
+  </section>
+  <!-- footer section -->
+
+  <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+  <script type="text/javascript" src="js/bootstrap.js"></script>
+
+  <script>
+    // This example adds a marker to indicate the position of Bondi Beach in Sydney,
+    // Australia.
+    function initMap() {
+      var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 11,
+        center: {
+          lat: 40.645037,
+          lng: -73.880224
+        },
+      });
+
+      var image = 'images/maps-and-flags.png';
+      var beachMarker = new google.maps.Marker({
+        position: {
+          lat: 40.645037,
+          lng: -73.880224
+        },
+        map: map,
+        icon: image
+      });
+    }
+  </script>
+  <!-- google map js -->
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA8eaHt9Dh5H57Zh0xVTqxVdBFCvFMqFjQ&callback=initMap">
+  </script>
+  <!-- end google map js -->
+</body>
+
+</html>
